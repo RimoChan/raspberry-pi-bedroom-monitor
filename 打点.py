ed5b83767cc7e201ea0d01744b8d292548e33508
RimoChan <RimoChan@stu.xmu.edu.cn>
2023-01-26 15:50:45 +0800

    打个ip

--- 打点.py
+++ 打点.py
@@ -1,8 +1,15 @@
+import socket
 from prometheus_client import start_http_server, Gauge
 
 
+_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+_s.connect(('1', 1))
+_local_ip_address = _s.getsockname()[0]
+
 gauge_dict = {}
 def save(key: str, value, tags: dict[str, str] = {}):
+    tags = tags.copy()
+    tags['local_ip'] = _local_ip_address
     if key not in gauge_dict:
         gauge_dict[key] = Gauge(f'pi_{key}', f'pi.{key}', labelnames=[*tags.keys()])
     if tags:
